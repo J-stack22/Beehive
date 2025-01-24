1281f8a02a7487b90382f96519ef0f7c69a8953c
Jem <andersonkids.ka@gmail.com>
2025-01-23 22:05:50 -0600

    Honeycomb
    The initial preview/commit to the Beehive password generator

---
+++ honeycomb.py
@@ -0,0 +1,74 @@
+import random
+import math
+
+word = input("Enter a memorable word of atleast 8 characters: ")
+
+p1 = "blank"
+n1 = "blank"
+
+
+text = word
+letter1 = text[3] 
+letter2 = text[4] 
+letter3 = text[5] 
+
+letter = letter1 + letter2 + letter3
+
+n1 = random.randrange(1,9)
+
+if (n1 == 1) :{
+
+print("Accordingtoallknownlawsofaviation,thereisnowayabeeshouldbeabletofly. Itswingsaretoosmalltogetitsfatlittlebodyofftheground. Thebee,ofcourse,fliesanywaybecausebeesdon'tcarewhathumansthinkisimpossible. Yellow,black.Yellow,black.Yellow,black.Yellow,black. Ooh,blackandyellow! Let'sshakeitupalittle. Barry!Breakfastisready! Coming! Hangonasecond. Hello? Barry? Adam? Canyoubelievethisishappening? Ican't. I'llpickyouup. Lookingsharp. Usethestairs,Yourfatherpaidgoodmoneyforthose. Sorry.I'mexcited. Here'sthegraduate. We'reveryproudofyou,son. Aperfectreportcard,allB's. Veryproud. Ma!Igotathinggoinghere. Yougotlintonyourfuzz. Ow!That'sme! Wavetous!We'llbeinrow118,000. Bye! Barry,Itoldyou,stopflyinginthehouse! Hey,Adam. Hey,Barry. Isthatfuzzgel? Alittle.Specialday,graduation. NeverthoughtI'dmakeit. Threedaysgradeschool,threedayshighschool. Thosewereawkward. Threedayscollege.I'mgladItookadayandhitchhikedaroundTheHive. Youdidcomebackdifferent. Hi,Barry.Artie,growingamustache?Looksgood. HearaboutFrankie? Yeah. Yougoingtothefuneral? No,I'mnotgoing. Everybodyknows,stingsomeone,youdie. Don'twasteitonasquirrel. Suchahothead. Iguesshecouldhavejustgottenoutoftheway. Ilovethisincorporatinganamusementparkintoourday. That'swhywedon'tneedvacations. Boy,quiteabitofpompunderthecircumstances. Well,Adam,todaywearemen. Weare! Bee-men. Amen! Hallelujah! Students,faculty,distinguishedbees, pleasewelcomeDeanBuzzwell. Welcome,NewHiveCitygraduatingclassof9:15. ThatconcludesourceremoniesAndbeginsyourcareeratHonexIndustries! Willwepickourjobtoday? Iheardit'sjustorientation. Headsup!Herewego.")
+
+}
+    
+if (n1 == 2) :{
+
+print("it's two")
+
+}
+    
+if (n1 == 3) :{
+
+print("it's three")
+
+}
+    
+if (n1 == 4) :{
+
+print("it's four")
+
+}
+    
+if (n1 == 5) :{
+
+print("it's five")
+
+}
+    
+if (n1 == 6) :{
+
+print("it's six")
+
+}
+    
+if (n1 == 7) :{
+
+print("it's seven")
+
+}
+    
+if (n1 == 8) :{
+
+print("it's eight")
+
+}
+    
+if (n1 == 9) :{
+
+print("it's nine")
+
+}
+
+
+
